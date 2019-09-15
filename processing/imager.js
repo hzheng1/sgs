56ba7f92957f2536d2ff8805a92a3986a6e81098
Kevin Zheng <hzheng1@stanford.edu>
2019-09-15 11:39:54 +0800

    Adding images

--- processing/imager.js
+++ processing/imager.js
@@ -39,13 +39,70 @@ http://sanguoshaenglish.blogspot.com/2010/08/zhua-huang-fei-dian.html
 http://sanguoshaenglish.blogspot.com/2010/08/zi-xing.html
 `.trim().split('\n');
 
+const heroes = `
+http://sanguoshaenglish.blogspot.com/2010/07/liu-bei.html
+http://sanguoshaenglish.blogspot.com/2010/07/elder-zhu-ge-liang.html
+http://sanguoshaenglish.blogspot.com/2010/07/zhao-yun.html
+http://sanguoshaenglish.blogspot.com/2010/07/guan-yu.html
+http://sanguoshaenglish.blogspot.com/2010/07/huang-yue-ying.html
+http://sanguoshaenglish.blogspot.com/2010/07/ma-chao.html
+http://sanguoshaenglish.blogspot.com/2010/07/zhang-fei.html
+http://sanguoshaenglish.blogspot.com/2010/08/younger-zhu-ge-liang.html
+http://sanguoshaenglish.blogspot.com/2010/07/pang-tong.html
+http://sanguoshaenglish.blogspot.com/2010/07/huang-zhong.html
+http://sanguoshaenglish.blogspot.com/2010/07/wei-yan.html
+http://sanguoshaenglish.blogspot.com/2010/09/meng-huo.html
+http://sanguoshaenglish.blogspot.com/2010/09/zhu-rong.html
+http://sanguoshaenglish.blogspot.com/2010/08/cao-cao.html
+http://sanguoshaenglish.blogspot.com/2010/08/guo-jia.html
+http://sanguoshaenglish.blogspot.com/2010/08/si-ma-yi.html
+http://sanguoshaenglish.blogspot.com/2010/08/xia-hou-dun.html
+http://sanguoshaenglish.blogspot.com/2010/08/xu-chu.html
+http://sanguoshaenglish.blogspot.com/2010/08/zhang-liao.html
+http://sanguoshaenglish.blogspot.com/2010/08/zhen-ji.html
+http://sanguoshaenglish.blogspot.com/2010/08/dian-wei.html
+http://sanguoshaenglish.blogspot.com/2010/08/xun-yu.html
+http://sanguoshaenglish.blogspot.com/2010/08/cao-ren.html
+http://sanguoshaenglish.blogspot.com/2010/08/xia-hou-yuan.html
+http://sanguoshaenglish.blogspot.com/2010/09/cao-pi.html
+http://sanguoshaenglish.blogspot.com/2010/09/xu-huang.html
+http://sanguoshaenglish.blogspot.com/2013/03/sun-quan.html
+http://sanguoshaenglish.blogspot.com/2013/03/da-qiao.html
+http://sanguoshaenglish.blogspot.com/2013/03/gan-ning.html
+http://sanguoshaenglish.blogspot.com/2013/03/huang-gai.html
+http://sanguoshaenglish.blogspot.com/2013/03/lu-meng.html
+http://sanguoshaenglish.blogspot.com/2013/03/lu-xun.html
+http://sanguoshaenglish.blogspot.com/2013/03/sun-shang-xiang.html
+http://sanguoshaenglish.blogspot.com/2013/03/zhou-yu.html
+http://sanguoshaenglish.blogspot.com/2013/03/tai-shi-ci.html
+http://sanguoshaenglish.blogspot.com/2013/03/xiao-qiao.html
+http://sanguoshaenglish.blogspot.com/2013/03/zhou-tai.html
+http://sanguoshaenglish.blogspot.com/2013/03/sun-jian.html
+http://sanguoshaenglish.blogspot.com/2013/03/lu-su.html
+http://sanguoshaenglish.blogspot.com/2010/08/diao-chan.html
+http://sanguoshaenglish.blogspot.com/2010/08/hua-tuo.html
+http://sanguoshaenglish.blogspot.com/2010/08/lu-bu.html
+http://sanguoshaenglish.blogspot.com/2010/08/yuan-shao.html
+http://sanguoshaenglish.blogspot.com/2010/08/pang-de.html
+http://sanguoshaenglish.blogspot.com/2010/08/yan-liang-wen-chou.html
+http://sanguoshaenglish.blogspot.com/2010/08/zhang-jiao.html
+http://sanguoshaenglish.blogspot.com/2010/08/yu-ji.html
+http://sanguoshaenglish.blogspot.com/2010/09/dong-zhuo.html
+http://sanguoshaenglish.blogspot.com/2010/09/jia-xu.html
+http://sanguoshaenglish.blogspot.com/2010/09/demi-god-zhu-ge-liang.html
+http://sanguoshaenglish.blogspot.com/2010/09/demi-god-zhou-yu.html
+http://sanguoshaenglish.blogspot.com/2010/09/demi-god-guan-yu.html
+http://sanguoshaenglish.blogspot.com/2010/09/demi-god-lu-meng.html
+http://sanguoshaenglish.blogspot.com/2010/09/demi-god-cao-cao.html
+http://sanguoshaenglish.blogspot.com/2010/09/demi-god-lu-bu.html
+`.trim().split('\n');
+
 const Jimp = require('jimp'),
 			http = require('http'),
 			fs = require('fs'),
 			request = require('request');
 
 cards.forEach((elem) => {
-	request.get({uri: elem}).pipe(fs.createWriteStream('./asdf.txt'));
 	request({
 		url: elem,
 		method: 'GET'
@@ -59,6 +116,23 @@ cards.forEach((elem) => {
 					img = img.rotate(90);
 				}
 				img.crop(30, 60, 225 - 60, 136).write(`../data/rawImages/cropped-${filename}`);
+			});
+		});
+	})
+});
+
+heroes.forEach((elem) => {
+	request({
+		url: elem,
+		method: 'GET'
+	}, (err, res, body) => {
+		const matches = body.match(/(http:\/\/[A-Za-z0-9-\_\.]*\.blogspot\.com\/[A-Za-z0-9\-_\/]+\/s320\/[a-z]+.jpg)/g);
+		const imageURL = matches[matches.length - 1];
+		console.log(imageURL);
+		const filename = imageURL.split('/').pop();
+		request(imageURL).pipe(fs.createWriteStream(`../data/heroImages/${filename}`)).on('close', () => {
+			Jimp.read(`../data/heroImages/${filename}`, (err, img) => {
+				img.crop(54, 32, 140, 210).write(`../data/heroImages/cropped-${filename}`);
 			});
 		});
 	})
